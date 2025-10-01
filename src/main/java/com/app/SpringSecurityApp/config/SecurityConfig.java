2429411f10b33050943ac7a901eb9f91f1869df2
Leandro HernandezSr <l.hernandez071999@gmail.com>
2025-10-01 12:26:25 -0300

    Add: se configura la clase securityConfig

--- src/main/java/com/app/SpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/app/SpringSecurityApp/config/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.app.SpringSecurityApp.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -13,9 +27,45 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    private final AuthenticationConfiguration authenticationConfiguration;
 
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration){
+        this.authenticationConfiguration=authenticationConfiguration;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable());
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception{
+        return this.authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider(userDetails());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetails(){
+        List<UserDetails> userDetails=new ArrayList<>();
+
+        userDetails.add(User.withUsername("lea").password("12345").roles("ADMIN").authorities("READ","CREATE").build());
+        userDetails.add(User.withUsername("lean").password("12345").roles("USER").authorities("READ").build());
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    
 }
