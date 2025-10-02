1839c9ae76c689fefab61f33c1ed05c979a17979
Leandro HernandezSr <l.hernandez071999@gmail.com>
2025-10-02 13:15:31 -0300

    Add: se agrega configuracion a SecurityFilterChain

--- src/main/java/com/app/SpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/app/SpringSecurityApp/config/SecurityConfig.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,7 +23,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -29,43 +30,55 @@ public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration){
-        this.authenticationConfiguration=authenticationConfiguration;
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf->csrf.disable());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .httpBasic(Customizer.withDefaults()).httpBasic(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(httpR->{
+                //Configuracion endpoints publicos
+                httpR.requestMatchers(HttpMethod.GET,"/auth/hello").permitAll();
+                //Configuracion de endpoints privados
+                httpR.requestMatchers(HttpMethod.GET,"/auth/hello-secured").hasAuthority("CREATE");
+                //Configurar el resto de endpoints --NO ESPECIFICADOS
+                httpR.anyRequest().denyAll();
+            });
+
         return http.build();
     }
 
+    
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception{
+    public AuthenticationManager authenticationManager() throws Exception {
         return this.authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider=new DaoAuthenticationProvider(userDetails());
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetails());
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
-    }
+    } 
 
     @Bean
-    public UserDetailsService userDetails(){
-        List<UserDetails> userDetails=new ArrayList<>();
+    public UserDetailsService userDetails() {
+        List<UserDetails> userDetails = new ArrayList<>();
 
-        userDetails.add(User.withUsername("lea").password("12345").roles("ADMIN").authorities("READ","CREATE").build());
+        userDetails
+                .add(User.withUsername("lea").password("12345").roles("ADMIN").authorities("READ", "CREATE").build());
         userDetails.add(User.withUsername("lean").password("12345").roles("USER").authorities("READ").build());
 
         return new InMemoryUserDetailsManager(userDetails);
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    
 }
